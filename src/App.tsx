64a84495966814ca29fe4ad9ca6506583a9f7cec
Sint Sint Hlaing <sints2001@gmail.com>
2022-10-15 11:52:40 +0630

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import List from './components/List'
+import AddToList from './components/AddToList'
+
+export interface InitialState  {
+  people : {
+    name : string
+    age : number
+    img : string
+    note ?: string
+  } []
+}
 
 function App() {
+  const [people,setPeople] = useState<InitialState["people"]>([
+    {
+      name: "John",
+      age: 25,
+      img: "https://img.freepik.com/free-photo/portrait-smiling-handsome-asian-man_1150-35750.jpg?size=338&ext=jpg&ga=GA1.2.1501776959.1661607449",
+      note: "Childhood friend",
+    },
+    {
+      name: "Emily",
+      age: 22,
+      img: "https://img.freepik.com/free-photo/beautiful-model-girl-with-perfect-fresh-clean-skin-youth-skin-care-concept-isolated-pastel-brown-background_658552-32.jpg?size=626&ext=jpg&ga=GA1.2.1501776959.1661607449",
+      note: "Roommate"
+    }
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Friend lists</h1>
+      <List people={people} />
+      <AddToList people={people} setPeople={setPeople} />
     </div>
   );
 }
